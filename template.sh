bedff2d2bdba73be7315114e632d7ac1c14a29ed
Luke Chen <doomer@yahoo.com>
2019-08-19 10:53:09 +0800

    fix error in template.sh

--- template.sh
+++ template.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 base=/path/spawn-web-request
-request=$base/path/request.sh
+request=/path/request.curl
 instance=2
-delay=0
+delay=1
 regex=keyword\|:code
 
 function fight() {
-	cmd="${base}/parallel_run.sh -n ${instance} -e "\'"${base}/replay.sh -f ${request} -d ${delay}"
+	cmd="${base}/parallel_run.sh -n ${instance} -e "\'"${base}/replay.sh -f ${request}"
+	if [ ! -z "$delay" ]; then
+		cmd="${cmd} -d ${delay}"
+	fi
 	if [ ! -z "$regex" ]; then
 		cmd="${cmd} -c ${regex}"
 	fi
