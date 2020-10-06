f83afca088b7243804bdbecf96e07346d90994ab
Luke Chen <doomer@yahoo.com>
2020-10-06 16:53:21 +0800

    update group template

--- template_group.sh
+++ template_group.sh
@@ -2,5 +2,7 @@
 
 request=/path/*.curl
 
-# give multiple request variables if want to run every request multiple times in parallel in a single round
-parallel 'echo {1/.}; date "+%Y/%m/%d %T.%3N"; sh {1}' ::: `ls -d $request $request` ::: {1..10}
+# give seq N if want to run every request N times in parallel in a single round
+repeat=`seq 10 | xargs -I -- ls -d $request`
+
+parallel 'echo {1/.}; date "+%Y/%m/%d %T.%3N"; sh {1}' ::: `ls -d $repeat` ::: {1..10}
